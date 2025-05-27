12069293db49f4dadd82cfd2da6bf1195ce90da3
laimonas2g <laimonas2g@gmail.com>
2025-05-27 21:20:13 +0300

    T1 90%

---
+++ ko nesupratau/011 js/app.js
@@ -0,0 +1,57 @@
+// parasyti dokumenta js i html
+
+
+// . means 'current directory'
+// {} means 'all files in current directory'
+// directory means 'directory name'
+// query means 'query string'
+// queryselector means 'query selector'
+// query selector (p+p) means 'select the first p element followed by another p element'
+// const means 'constant variable'
+// let means 'variable that can be changed'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
