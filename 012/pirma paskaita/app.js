13f7c37955783b19332552cccca9b1e0ae17987d
laimonas2g <laimonas2g@gmail.com>
2025-05-28 15:59:12 +0300

    2 paskaita

---
+++ 012/pirma paskaita/app.js
@@ -0,0 +1,48 @@
+
+let medis = {};// objektas, kuris turi savybes ir funkcijas
+
+medis.tipas = 'Liepa';
+medis.aukstis = 24;
+medis.matavimoVienetai = 'm';
+medis.amzius = {};
+medis.amzius.reiksme = 25;
+medis.amzius.matavimas = 'metai';
+
+console.log(medis);
+
+
+
+let automobilis = {};
+
+automobilis.tipas = 'Liepa';
+automobilis.aukstis = 24;
+automobilis.matavimoVienetai = 'm';
+automobilis.amzius = {};
+automobilis.amzius.reiksme = 25;
+automobilis.amzius.matavimas = 'metai';
+
+console.log (automobilis);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nd/01/app.js
@@ -0,0 +1,51 @@
+// 1.
+
+const h1 = document.querySelector('h1');
+h1.innerText = 1;
+h1.style.color = 'crimson';
+
+const h2 = document.querySelector('h2');
+h2.innerText = 2;
+
+const h3 = document.querySelector('h3');
+h3.innerText = 3;
+h3.style.color = 'crimson';
+
+const h4 = document.querySelector('h4');
+h4.innerText = 4;
+h4.style.color = 'blue';
+
+// 2.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
