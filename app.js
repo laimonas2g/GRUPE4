1049da490f0a17247a6e4ed32eb0755c4075ede5
laimonas2g <laimonas2g@gmail.com>
2025-05-28 18:41:41 +0300

    nd 01 1, 2.

---
+++ app.js
@@ -0,0 +1,2 @@
+
+// let (div + span + span + span)
--- nd/01/app.js
+++ nd/01/app.js
@@ -1,34 +1,65 @@
 // 1.
 
-const h1 = document.querySelector('h1');
+let h1 = document.querySelector('h1');
 h1.innerText = 1;
 h1.style.color = 'crimson';
 
-const h2 = document.querySelector('h2');
+let h2 = document.querySelector('h2');
 h2.innerText = 2;
 
-const h3 = document.querySelector('h3');
+let h3 = document.querySelector('h3');
 h3.innerText = 3;
 h3.style.color = 'crimson';
 
-const h4 = document.querySelector('h4');
+let h4 = document.querySelector('h4');
 h4.innerText = 4;
 h4.style.color = 'blue';
 
+console.log('h1');
+
 // 2.
 
-const keturkampis = document.querySelector('div');
+let keturkampis = document.querySelector('code + div');
+keturkampis.style.width = '150px';
+keturkampis.style.height = '150px';
+keturkampis.style.backgroundColor = 'blue';
+console.log('keturkampis');
+
+let apskritimas = document.querySelector('code + div + div');
+apskritimas.style.width = '150px';
+apskritimas.style.height = '150px';
+apskritimas.style.backgroundColor = 'red';
+apskritimas.style.borderRadius = '50%';
+console.log('apskritimas');
+
+// 3.
 
 
+// let pirmasKint = function rand(min, max) {
+//     const minCeiled = Math.ceil(min);
+//     const maxFloored = Math.floor(max);
+//     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+// }
+
+// console.log(pirmasKint);
+
+// let antrasKint = function random(number) {
+//   const result = Math.floor(Math.random() * number);
+//   return result;
+// }
+
+// console.log(antrasKint);
 
 
+// 9. #go9
+// 2 X 8 = console.log(2*8);
+// 3 X 3 = console.log(3*3);
+// 1 X 3 =
+// 3 X 6 =
+// 3 X 4 =
 
-
-
-
-
-
-
+// let daugyba1 = document.querySelector('#go9')
+// console.log('2*9 = 18');
 
 
 
