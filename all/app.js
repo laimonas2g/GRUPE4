e43655b7deffd8b9cdbd623979d526e4224d0e80
laimonas2g <laimonas2g@gmail.com>
2025-06-04 23:10:07 +0300

    nd03_+6kl

--- all/app.js
+++ all/app.js
@@ -1,20 +1,127 @@
-function rand(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-}
 
-const numbers = [];
-for (let i = 0; i < 44; i++) {
-  numbers.push(rand(14, 44));
-}
 
-const coloredNumbers = numbers.map(num => {
-  const color = num % 4 === 0 ? 'red' : 'blue';
-  return `<span style="color:${color}">${num}</span>`;
-});
+// Yra zodis "divagate". Reikia su for ciklu pereiti kiekviena raide ir 
+// konsoleje spausdinti tik tada jeigu raide yra a
 
-document.body.innerHTML = coloredNumbers.join(' ');
+// Yra zodis "afforest". Reikia su for ciklu pereiti kiekviena raide ir 
+// konsoleje spausdinti tik tada jeigu raide yra f
+
+// Yra zodis "aftermath". Reikia su for ciklu pereiti kiekviena raide ir 
+// konsoleje spausdinti tik tada jeigu raide yra a
+
+// Yra zodis "blithesome". Reikia su for ciklu pereiti kiekviena raide ir 
+// konsoleje spausdinti tik tada jeigu raide yra e
+
+// Yra zodis "buffoonish". Reikia su for ciklu pereiti kiekviena raide ir 
+// konsoleje spausdinti tik tada jeigu raide yra f
+
+// Yra zodis "eucatastrophe". Reikia su for ciklu pereiti kiekviena raide ir 
+// konsoleje spausdinti tik tada jeigu raide yra t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const zodis1 = `divagate`;
+// console.log(zodis1);
+
+// for (let index = 0; index < zodis1.length; index++) {
+//  if (zodis1[index] == `a`) console.log(zodis1[index])
+// }
+
+// const d1 = `divagate`;
+// console.log(d1)
+
+// for (let index = 0; index < d1.length; index++ ) {
+//   if (d1[index] == `a`); console.log(d1[index])
+// }
+
+// const p1 = document.createElement(`p`);
+// p1.innerText = 3;
+// p1.id = 'p1'; // Assign an ID for selection
+// document.body.append(p1)
+
+// const p2 = document.createElement(`span`);
+// p2.innerText = 7;
+// p2.id = 'p2'; // Assign an ID for selection
+// document.body.append(p2);
+
+// const pirmasSkaicius = document.querySelector(`#p1`);
+// const antrasSkaicius = document.querySelector(`#p2`);
+
+// // Extract numbers from innerText and convert to numbers before multiplying
+// const skaicius1 = Number(pirmasSkaicius.innerText);
+// const skaicius2 = Number(antrasSkaicius.innerText);
+
+// const suma = skaicius1 * skaicius2;
+// console.log(suma)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const h2 = document.createElement('h2');
+// h2.innerText = 'Hello';
+// document.body.appendChild(h2);
+
+
+
+
+
+
+
+// const b = `Ananasas`;
+
+// for (let ii = 0; ii < b.length ; ii++) 
+
+//  if (b[ii] == `a`) {
+//   console.log(b[ii]);
+//  } 
+
 
 
 
--- nd/03/app.js
+++ nd/03/app.js
@@ -38,23 +38,38 @@ const C = [
 ///////////// 1. /////////////
 // Html faile sukurkite ul tagą (tiesiogiai). 
 // Į sukurtą tagą, su JS, sudėkite li tagus su iš masyvo A nuskaitytais gyvūnais.
+
 console.log(A, A.length);
 
-let GyvLis = '';
 
-for (let i = 0; i < A.length; i++) {
-  GyvLis += '<li>' + A[i] + '</li>';
+let GyvList1 = '';
+
+for (let index = 0; index < A.length; index++) {
+  GyvList1 += '<li>' + A[index] + '</li>'; 
 }
+console.log(GyvList1);
+console.log(A[1], A[2])
 
-console.log(GyvLis);
-
-const ul = document.querySelector('ul'); 
-ul.innerHTML = GyvLis;
+const kl1 = document.querySelector(`.klausimas1`); 
+console.log(typeof kl1)
+kl1.innerHTML = GyvList1;
 
 ///////////// 2. /////////////
 // Html faile sukurkite ul tagą (tiesiogiai). 
 // Į sukurtą tagą, su JS, sudėkite li tagus su iš masyvo A 
 // nuskaitytais gyvūnais išdėliotais abėcėline tvarka.
+
+let GyvList2 = '';
+
+A.sort();
+
+for (let index = 0; index < A.length; index++) {
+    GyvList2 += '<li>' + A[index] + '</li>'; 
+}
+let kl2 = document.querySelector(`.klausimas2`);
+
+kl2.innerHTML = GyvList2;
+
 
 ///////////// 3. /////////////
 // Html faile sukurkite ul tagą (tiesiogiai). 
@@ -62,28 +77,139 @@ ul.innerHTML = GyvLis;
 // masyvų A ir B nuskaitytais gyvūnais išdėliotais abėcėline tvarka 
 // (Rūšiavimas turi būti bendras tarp abiejų masyvų, NE kiekvieno atskirai).
 
+let GyvList3 = '';
+
+let AB = [...A, ...B].sort();
+
+for (let index = 0; index < AB.length; index++) {
+    GyvList3 += '<li>' + AB[index] + '</li>';
+}
+
+let kl3 = document.querySelector(`.klausimas3`);
+kl3.innerHTML = GyvList3;
+
 ////////////// 4. //////////////
 // Html faile sukurkite section tagą (tiesiogiai). 
 // Į sukurtą tagą, su JS, sudėkite div tagus, 
 // kurie yra mėlyni apskritimai su centre užrašytais iš masyvo A nuskaitytais gyvūnais.
 
+let section4 = document.querySelector('.klausimas4');
+let div4 = document.createElement(`div`);
+div4.innerHTML = GyvList1; // .innerHTML kai viduje .createElement neletina browserio?
+section4.append(div4);
+div4.style.width = `150px`;
+div4.style.height = `150px`;
+div4.style.borderRadius = `50%`;
+div4.style.backgroundColor = `skyblue`;
+div4.style.display = `flex`;
+div4.style.justifyContent = `center`;
+div4.style.flexDirection = `column`;
+div4.style.padding = `100px`
+
+// let div4 = document.createElement(`div`);
+// div4.innerHTML = GyvList1; 
+// document.body.append(div4)
 
 ///////////// 5. /////////////
+// Html faile sukurkite section tagą (tiesiogiai). 
+// Į sukurtą tagą, su JS, sudėkite div tagus, 
+// kurie yra raudoni apskritimai su centre centre užrašytais 
+// iš masyvo B nuskaitytais gyvūnais ir tų gyvūnų raidžių skaičiumi.
+
+console.log(B.length);
+let section5 = document.querySelector('.klausimas5');
+
+let div5_1 = document.createElement(`div`);
+div5_1.innerHTML = A[0] + ' ' + A[0].length;
+
+section5.append(div5_1)
+section5.style.display = `flex`;
+
+section5.style.gap = `5px`;
+div5_1.style.width = `57px`;
+div5_1.style.height = `57px`;
+div5_1.style.borderRadius = `50%`;
+div5_1.style.backgroundColor = `#f63737`;
+div5_1.style.display = `flex`;
+div5_1.style.color = `#black`;
+div5_1.style.justifyContent = `center`;
+div5_1.style.flexDirection = `column`;
+div5_1.style.padding = `10px`
+
+let div5_2 = document.createElement(`div`);
+div5_2.innerHTML = A[1] + ' ' + A[1].length;
+section5.append(div5_2)
+
+div5_2.style.width = `57px`;
+div5_2.style.height = `57px`;
+div5_2.style.borderRadius = `50%`;
+div5_2.style.backgroundColor = `#f63737`;
+div5_2.style.display = `flex`;
+div5_2.style.color = `#black`;
+div5_2.style.justifyContent = `center`;
+div5_2.style.flexDirection = `column`;
+div5_2.style.padding = `10px`
+
+let div5_3 = document.createElement(`div`);
+div5_3.innerHTML = A[2] + ' ' + A[2].length;
+section5.append(div5_3)
+
+div5_3.style.width = `57px`;
+div5_3.style.height = `57px`;
+div5_3.style.borderRadius = `50%`;
+div5_3.style.backgroundColor = `#f63737`;
+div5_3.style.display = `flex`;
+div5_3.style.color = `#black`;
+div5_3.style.justifyContent = `center`;
+div5_3.style.flexDirection = `column`;
+div5_3.style.padding = `10px`
+
+let div5_4 = document.createElement(`div`);
+div5_4.innerHTML = A[3] + ' ' + A[3].length;
+section5.append(div5_4)
+
+div5_4.style.width = `57px`;
+div5_4.style.height = `57px`;
+div5_4.style.borderRadius = `50%`;
+div5_4.style.backgroundColor = `#f63737`;
+div5_4.style.display = `flex`;
+div5_4.style.color = `#black`;
+div5_4.style.justifyContent = `center`;
+div5_4.style.flexDirection = `column`;
+div5_4.style.padding = `10px`
+
+let div5_5 = document.createElement(`div`);
+div5_5.innerHTML = A[4] + ' ' + A[4].length;
+section5.append(div5_5)
+
+div5_5.style.width = `57px`;
+div5_5.style.height = `57px`;
+div5_5.style.borderRadius = `50%`;
+div5_5.style.backgroundColor = `#f63737`;
+div5_5.style.display = `flex`;
+div5_5.style.color = `#black`;
+div5_5.style.justifyContent = `center`;
+div5_5.style.flexDirection = `column`;
+div5_5.style.padding = `10px`
 
 
 ///////////// 6. /////////////
+// Html faile sukurkite section tagą (tiesiogiai). 
+// Į sukurtą tagą, su JS, sudėkite div tagus, kurie yra žali apskritimai su centre užrašytais gyvūnais, 
+// nuskaityto iš masyvo A. Dėkite tik tuos gyvūnus, kurie savo pavadinime turi tik vieną žodį.
+
+
 
 
 ///////////// 7. /////////////
+// Html faile sukurkite section tagą (tiesiogiai). Į sukurtą tagą, su JS, sudėkite span tagus, 
+// kurie yra geltoni apskritimai su centre užrašytais iš masyvo B nuskaitytų gyvūnų pavadinimų raidėm. 
+// Kiekvienas span tagas- atskira raidė. (visų gyvūnų visos raidės atskiruose span taguose).#ciklasCikle
 
 
-///////////// 8. /////////////
 
 
-///////////// 9. /////////////
 
-
-///////////// 10. /////////////
 
 
 
