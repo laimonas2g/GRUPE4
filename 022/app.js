ac9f3c74a4d867bfc48bef4c20a6b439e889b06f
laimonas2g <laimonas2g@gmail.com>
2025-06-17 14:04:18 +0300

    023_3pask

--- 022/app.js
+++ 022/app.js
@@ -115,7 +115,24 @@ if (cartProduct) {
 
 console.log(cartProduct)
 
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json()) // kai sulaukiam atsakyma paverciam objektu
+  .then(json => {
+    console.log(json);
+    printTitles(json); // sukurtas variable ateityje, 34 eilute
+  }) // atspausdinam konsoleje
 
+console.log('kodas po fetch');  // nelaukia response ir spausdina
+
+const printTitles = obj => { 
+  const ul = document.querySelector('ul');
+
+  obj.forEach(p => {
+    const li = document.createElement('li');
+    li.innerText = p.title;
+    ul.appendChild(li);
+  });
+}
 
 
 
--- 023/Antra paskaita/app.js
+++ 023/Antra paskaita/app.js
@@ -1,39 +1,42 @@
-console.log('Labas, Filtre!')
-
+console.log('Bebrų dainos');
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json()) // kai sulaukiam atsakyma paverciam objektu
-  .then(json => {
-    // console.log(json);
-    printUsers(json); // sukurtas variable ateityje, 34 eilute
-  }) // atspausdinam konsoleje
+    .then(response => response.json())
+    .then(json => {
+        // console.log(json);
+        printUsers(json);
+    });
 
-console.log('kodas po fetch');  // nelaukia response ir spausdina
 
-    const printUsers = users => {
-        const ul = document.querySelector('ul');
-        users.forEach(user => {
-            // const li = document.createElement('li');
-            // li.textContent = `${user.name} (${user.email}) from ${user.address.city}`;
-            // ul.appendChild(li);
-            const template = document.querySelector('#user-template');
-            const clone = template.content.cloneNode(true); //padaro atskira kopija
-            clone.querySelector('.name').textContent = user.name;
-            clone.querySelector('.email').textContent = user.email;
-            clone.querySelector('.city').textContent = user.address.city;
-            ul.appendChild(clone);
-        });
-    }
+const printUsers = users => {
+    const ul = document.querySelector('ul');
+    users.forEach(user => {
+        // const li = document.createElement('li');
+        // li.textContent = `${user.name} (${user.email}) from ${user.address.city}`;
+        // ul.appendChild(li);
+        const template = document.querySelector('#user-template');
+        const clone = template.content.cloneNode(true); // padaro atskirą kopiją
+        clone.querySelector('.name').textContent = user.name;
+        clone.querySelector('.email').textContent = user.email;
+        clone.querySelector('.city').textContent = user.address.city;
+        ul.appendChild(clone);
+    });
+}
 
 const cart = [
     { id: 877, title: 'Pieniškos dešros', price: 2.45, count: 2 },
     { id: 8974, title: 'Sviestukas sintetinis', price: 3.20, count: 1 },
     { id: 16548, title: 'Batonas studentų', price: 1.22, count: 1 },
-    { id: 974, title: 'Bulvės 4kl', price: 0.88, count: 2 }
+    { id: 974, title: 'Bulvės 4kl', price: 0.88, count: 2 },
+    { id: 7841, title: 'Žąsinas užšaldytas', price: 8.99, count: 1 },
+    { id: 785, title: 'Šašlykas marinuotas', price: 4.88, count: 1 },
+    { id: 7854, title: 'Zuikis netikras', price: 5.22, count: 1 },
+    { id: 1223, title: 'Ąsotis kačių pienui laikytt', price: 45.99, count: 1 }
 ];
 
-const batonas = cart.find(p => p.id == 16548)
+
+const batonas = cart.find(p => p.id == 16548);
 
 console.log(batonas);
 
@@ -41,21 +44,54 @@ const noDesra = cart.filter(p => p.id != 877);
 
 console.log(noDesra);
 
-let cartCount = 0;
-cart.forEach(p => cartCount = cartCount + p.count );
 
-const cartCount2 = cart.reduce((acc, p) => acc + p.count, 0)
+let cartCount = 0;
+cart.forEach(p => cartCount = cartCount + p.count);
+
+
+const cartCount2 = cart.reduce((acc, p) => acc + p.count, 0);
 
 
 console.log(cartCount, cartCount2);
 
+cart.sort((a, b) => b.price - a.price);
 
+const cartSort = cart.toSorted((a, b) => a.price - b.price);
 
+console.log(cart, cartSort);
 
+const cartByTitle = cart.toSorted((a, b) => {
+    if (a.title > b.title) {
+        return 1;
+    }
+    if (a.title < b.title) {
+        return -1;
+    }
+    return 0;
+});
 
+const cartByTitleLocal = cart.toSorted((a, b) => a.title.localeCompare(b.title, 'lt'));
 
+console.log(cartByTitle);
+console.log(cartByTitleLocal);
 
+console.clear();
 
+// https://jsonplaceholder.typicode.com/todos
+// 1. Parsiųsti ir atspausdinti KONSOLĖJE
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(data => {
+        data.sort((a, b) => a.title.localeCompare(b.title));
+        // console.log(data);
+        // console.log(data.filter(t => t.completed));
+        console.log(data.filter(t => t.userId == 7));
+    });
+
+// 2. Spausdinti TIK užbaigtus TODUS
+// 3. Spausdinti TIK 7 vartojo TODUS
+// 4. VISUS TODUS atspausdinti išrūšiuotus pagal pavadinimą nuo A iki Z
 
 
 
