12a5d9e3d076060f59408553c18e14f3af1f40bc
laimonas2g <laimonas2g@gmail.com>
2025-06-26 10:08:13 +0300

    nd04_end

---
+++ nd/05/app.js
@@ -0,0 +1,151 @@
+console.log('Kibiras')
+
+// Question-1: Sukurti klasę Kibiras1. Konstruktoriuje sukurti savybę akmenuKiekis  kuri lygi 0. 
+// Parašyti šiai klasei metodus, pridedančius akmenis: prideti1Akmeni() pridetiDaugAkmenu(kiekis) 
+// ir metodą išvedantį akmenų kiekį į konsolę- kiekPririnktaAkmenu(). Sukurti vieną kibiro objektą 
+// ir pademonstruoti akmenų rinkimą į kibirą ir rezultatų išvedimą.
+
+// class Kibiras1 {
+
+//     constructor(kiekis) {
+//         this.akmenuKiekis = 0;
+//         this.kiekis = kiekis;
+//     }
+//     prideti1Akmeni() {
+//         return this.akmenuKiekis + 1;
+//     }
+
+//     pridetiDaugAkmenu() {
+//         return this.akmenuKiekis + this.kiekis;
+//     }
+//     kiekPririnktaAkmenu() {
+//         console.log(this.pridetiDaugAkmenu);
+//     }
+// }
+
+// const Kibiras2 = new Kibiras1();
+
+// kibiras2.pridetiDaugAkmenu(3);
+
+class Kibiras1 {
+    constructor() {
+        this.akmenuKiekis = 0;
+    }
+
+    prideti1Akmeni() {
+        this.akmenuKiekis = this.akmenuKiekis + 1;
+    }
+
+    pridetiDaugAkmenu(kiekis) {
+        this.akmenuKiekis = this.akmenuKiekis + kiekis;
+    }
+
+    kiekPririnktaAkmenu() {
+        console.log(this.akmenuKiekis);
+    }
+}
+
+const kibiras2 = new Kibiras1();
+kibiras2.prideti1Akmeni();
+kibiras2.pridetiDaugAkmenu(3);
+kibiras2.kiekPririnktaAkmenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const preke1 = {
+//   title: 'Plastikines samanos',
+//   price: 45.4,
+//   amount: 5,
+//   cat: 'Bebro prekes',
+//   vat: function () {
+//     return this.price * this.amount / 100 * 21;
+//   },
+//   total: function () {
+//     return (this.price * this.amount + this.vat()).toFixed(2);
+//   }
+// }
+
+// const preke2 = {
+//   title: 'Guminiai robotai',
+//   price: 158.2,
+//   amount: 3,
+//   cat: 'Bebro prekes',
+//   vat: function () {
+//     return this.price * this.amount / 100 * 21;
+//   },
+//   total: function () {
+//     return (this.price * this.amount + this.vat()).toFixed(2);
+//   }
+
+// console.log(preke1.title, preke1.total());
+// console.log(preke2.title, preke2.total());
+
+
+// class BebroPreke {
+
+//   constructor(pavadinimas, kaina, kiekis) {
+//     this.title = pavadinimas;
+//     this.price = kaina;
+//     this.amount = kiekis;
+//     this.cat = 'Bebro prekes';
+//   }
+//   vat() {
+//     return this.price * this.amount / 100 * 21;
+//   }
+//   total() {
+//     return (this.price * this.amount + this.vat()).toFixed(2);
+//   }
+// };
+
+// const preke3 = new BebroPreke('Plastikiniai maisai', 0.42, 10);
+// const preke4 = new BebroPreke('Dziutiniai maisai', 0.77, 10);
+
+// console.log(preke1, preke1.total());
+// console.log(preke2, preke2.total());
+// console.log(preke3, preke3.total());
+// console.log(preke4, preke4.total());
+
+// class Du {
+
+//   constructor(t) { // pasileidzia pati
+//     this.kiek = 2;
+//     this.turi = t;
+//   }
+
+//   daug() {  // getter, funkcija kuri grazina, gaunu
+//     return this.kiek * this.turi;
+//   }
+
+//   keistiKiek(k) { // setter, irasyt i objekta
+//     if (k >= 10) {
+//       this.kiek = k;
+//     }
+//   }
+// }
+
+// // ONA sukure, objekto kurimo metas
+// const du01 = new Du(55);
+// const du02 = new Du(33);
+
+// du01.keistiKiek(40);
+
+// console.log(du01.kiek);
+
+
+
+
+
+
+
