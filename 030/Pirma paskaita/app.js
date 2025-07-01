19cdf8a6cda729a044677005ef1159c131b6e41f
laimonas2g <laimonas2g@gmail.com>
2025-07-01 10:52:08 +0300

    laikrodis1

--- 030/Pirma paskaita/app.js
+++ 030/Pirma paskaita/app.js
@@ -1,3 +1,73 @@
+/* Susirenkam DOM elementus pradziai */
+
+const dateEl = document.getElementsByClassName('date')[0]; //grazins mums nod lista, reik su [0] pasiselektint is saraso
+const timeEl = document.getElementsByClassName('time')[0];
+
+const weekdays = [
+  "Sekmadienis",
+  "Pirmadienis",
+  "Antradienis",
+  "Trečiadienis",
+  "Ketvirtadienis",
+  "Penktadienis",
+  "Šeštadienis",
+];
+
+const months = [
+  "Sausio",
+  "Vasario",
+  "Kovo",
+  "Balandžio",
+  "Gegužės",
+  "Birželio",
+  "Liepos",
+  "Rugpjūčio",
+  "Rugsėjo",
+  "Spalio",
+  "Lapkričio",
+  "Gruodžio",
+];
+
+
+function clock() {
+  const now = new Date();
+
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+
+  h = addZero(h);
+  m = addZero(m);
+  s = addZero(s);
+
+  let month = now.getMonth()
+  let weekday = now.getDay();
+  let day = now.getDate()
+
+  timeEl.textContent = h + ':' + m + ':' + s;
+  setTimeout(clock, 1000)
+
+  dateEl.textContent = weekdays[weekday] + ', ' + months[month] + ' ' + day + ' d.';
+}
+
+function addZero(t) {
+  if (t < 10) {
+    t = '0' + t;
+  }
+  return t;
+}
+
+clock();
+
+
+
+
+
+
+
+
+
+
 
 
 
