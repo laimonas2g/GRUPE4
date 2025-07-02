d20e7d9c466fc25fa5ea7bec3fa29714e1983e6f
laimonas2g <laimonas2g@gmail.com>
2025-07-02 14:11:37 +0300

    do_while

--- 031/Antra paskaita/app.js
+++ 031/Antra paskaita/app.js
@@ -5,7 +5,7 @@ function rand(min, max) {
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
- 
+
 /*
 
 kai reiskia suskaiciuoti kartus kazkieno
@@ -29,7 +29,7 @@ break
 for (let i = 0; i < 7; i++) {
   const random = rand(10, 99);
 
-  if(random > 50) {
+  if (random > 50) {
     continue; // nevykes pavadinimas, peršoka ir pradeda is naujo iki kito jump
   }
 
@@ -41,7 +41,7 @@ console.log('-------------------')
 for (let i = 0; i < 7; i++) {
   const random = rand(10, 99);
 
-  if(random > 70) {
+  if (random > 70) {
     break; // nutraukia cikla
   }
 
@@ -50,21 +50,85 @@ for (let i = 0; i < 7; i++) {
 
 console.clear();
 
-for (let i = 0; i < 3; i++) {
+// for (let i = 0; i < 30; i++) {
+
+//   const HS = rand(0, 1) ? 'H' : 'S';
+//   console.log(HS);
+
+//   // if (HS == 'H') { labai blogai
+//   //   break;
+//   // }
+// }
+
+console.clear();
+
+let HS1;
+let HS2;
+let saugiklis = 88;
+
+do {
+  if (--saugiklis == 0) {  // rankinis stabdis
+    console.log('Klaida, uzsiciklinai!!!');
+    break;
+  }
+
+
+  HS1 = rand(0, 1) ? 'H' : 'S';
+  HS2 = rand(0, 1) ? 'H' : 'S';
+  //   const HS1 = rand(0, 1) ? 'H' : 'S';
+  // // const HS2 = rand(0, 1) ? 'H' : 'S';
+  // console.log(HS1, HS2);
+
+} while (HS1 != 'H' || HS2 != 'H');
+// } while (HS1 != 'H' && HS2 != 'H');
+
+/*
+
+<-> - verciam
+
+== <-> !=
+> <-> <=
+> <-> >=
+|| <-> &&
+
+
+*/
+// Meskite monetą tol, kol tris kartus iškris herbas
+
+let HS3;
+let saugiklis3 = 100;
+
+do {
+  if (--saugiklis3 == 0) {
+    console.log('Klaida, uzsiciklinai!!!');
+    break;
+  }
+
+  HS1 = rand(0, 1) ? 'H' : 'S';
+  HS2 = rand(0, 1) ? 'H' : 'S';
+  HS3 = rand(0, 1) ? 'H' : 'S';
+  console.log(HS1, HS2, HS3);
+
+} while (HS1 != 'H' || HS2 != 'H' || HS3 != 'H');
+
+// Meskite monetą tol, kol iš eilės tris kartus iškris herbas
+
+let kiekis = 0;
+saugiklis = 10000;
+
+do {
+    if (--saugiklis3 == 0) {
+    console.log('BUM');
+    break;
+  }
 
   const HS = rand(0, 1) ? 'H' : 'S';
   console.log(HS);
+  if (HS == 'H') {
+    kiekis++;
+  }
 
-}
-
-
-
-
-
-
-
-
-
+} while (kiekis != 3);
 
 
 
