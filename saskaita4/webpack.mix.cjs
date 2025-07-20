7b7bedbe00bd9625fe35ceef13a145dd54b28107
laimonas2g <laimonas2g@gmail.com>
2025-07-20 16:31:29 +0300

    sask4 v1.03

--- saskaita4/webpack.mix.cjs
+++ saskaita4/webpack.mix.cjs
@@ -1,3 +1,5 @@
 const mix = require('laravel-mix');
-mix.js('src/app.js', 'public/js').sass('src/style.scss', 'public/css').setPublicPath('public');
+mix.js('src/app.js', 'public/js')
+   .sass('src/style.scss', 'public/css')
+   .setPublicPath('public');
 mix.disableNotifications();
